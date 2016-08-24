6885ee829621d050f35aebc46ba26ca4cd5aad54
Carlos Gavino <cs.gavino@gmail.com>
2016-08-24 17:01:15 +0800

    First pass at adding things

--- app/src/main/java/com/example/sandbox2/ColorsAdapter.kt
+++ app/src/main/java/com/example/sandbox2/ColorsAdapter.kt
@@ -5,8 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import java.util.*
 
-val colors: List<Long> = listOf(
+val colors: MutableList<Long> = mutableListOf(
         0xff00bcd4, 0xff3f51b5, 0xff4285f4, 0xffe91e63, 0xff0f9d58, 0xff8bc34a, 0xffff9800,
         0xffff5722, 0xff9e9e9e, 0xff00796b, 0xff00695c, 0xff3367d6, 0xff2a56c6, 0xff303f9f,
         0xff283593, 0xff7b1fa2, 0xff6a1b9a, 0xffc2185b, 0xff00bcd4, 0xff3f51b5, 0xff4285f4,
@@ -17,7 +18,7 @@ val colors: List<Long> = listOf(
         0xff303f9f, 0xff283593, 0xff7b1fa2, 0xff6a1b9a, 0xffc2185b
 )
 
-class ColorsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ColorsViewHolder(itemView: View, val callback: ColorsCallback? = null) : RecyclerView.ViewHolder(itemView) {
     fun bind(hex: Long) {
         bind(hex.toInt()) // Really
     }
@@ -27,23 +28,40 @@ class ColorsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         val hexTextView = itemView.findViewById(R.id.hexTextView) as TextView
         hexTextView.text = "#" + Integer.toHexString(hex)
+
+        itemView.setOnClickListener {
+            callback?.onClick(itemView)
+        }
     }
 }
 
-class ColorsAdapter : RecyclerView.Adapter<ColorsViewHolder>() {
-    override fun onBindViewHolder(holder: ColorsViewHolder, position: Int) {
-        holder.bind(colors[position])
-    }
+interface ColorsCallback {
+    fun onClick(itemView: View)
+}
 
-    override fun getItemCount(): Int {
-        return colors.count()
-    }
+class ColorsAdapter : RecyclerView.Adapter<ColorsViewHolder>() {
+    private var callback: ColorsCallback? = null
+
+    override fun onBindViewHolder(holder: ColorsViewHolder, position: Int) = holder.bind(colors[position])
+
+    override fun getItemCount(): Int = colors.count()
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ColorsViewHolder {
         val v = LayoutInflater.from(parent!!.context)
                 .inflate(R.layout.item_color, parent, false)
 
-        return ColorsViewHolder(v)
+        return ColorsViewHolder(v, callback)
+    }
+
+    fun setCallback(callback: ColorsCallback) {
+        this.callback = callback
+    }
+
+    fun addsThings() {
+        val position = 0
+
+        colors.add(position, 0xff00bcd4)
+        notifyItemInserted(position)
     }
 
 }
--- app/src/main/java/com/example/sandbox2/ColorsFragment.kt
+++ app/src/main/java/com/example/sandbox2/ColorsFragment.kt
@@ -8,24 +8,36 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
 
-
-class ColorsFragment : Fragment() {
+class ColorsFragment : Fragment(), ColorsCallback {
 
     lateinit var recyclerView: RecyclerView
+    lateinit var radioGroup: RadioGroup
+
+    val colorsAdapter = ColorsAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.fragment_colors, container, false)
 
-        recyclerView = view.findViewById(R.id.colors) as RecyclerView
+        radioGroup = view.findViewById(R.id.radioGroup) as RadioGroup
 
+        recyclerView = view.findViewById(R.id.colors) as RecyclerView
         recyclerView.layoutManager = LinearLayoutManager(view.context)
-        recyclerView.adapter = ColorsAdapter()
+        recyclerView.adapter = colorsAdapter
+
+        colorsAdapter.setCallback(this)
 
         return view
     }
 
+    override fun onClick(itemView: View) = when (radioGroup.checkedRadioButtonId) {
+        R.id.add_btn -> colorsAdapter.addsThings()
+        R.id.change_btn -> colorsAdapter.addsThings()
+        R.id.delete_btn -> colorsAdapter.addsThings()
+        else -> throw IllegalArgumentException("invalid resource")
+    }
 
 }
